994be6958ba04f947d929546ad6060753a5bb69d
mukulgoyal <mukulgoyal@urbanclap.com>
2019-05-25 11:03:36 +0530

    added gg dashboard

---
+++ precache-manifest.467fbd5cc2b74086dac7ca213a17c05b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42de2ee298bc451c421cea477cd7dd4a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7729c8bebbb30b0e450e",
+    "url": "/static/css/2.2a88c607.chunk.css"
+  },
+  {
+    "revision": "a63e792376e21731c107",
+    "url": "/static/css/main.e093f571.chunk.css"
+  },
+  {
+    "revision": "7729c8bebbb30b0e450e",
+    "url": "/static/js/2.3d5bb72d.chunk.js"
+  },
+  {
+    "revision": "a63e792376e21731c107",
+    "url": "/static/js/main.88942dcf.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
